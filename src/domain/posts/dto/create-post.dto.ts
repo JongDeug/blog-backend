8998fb1fe02b92756effcfc9423714459cf9abf5
JongDeug <jongdeug2021@gmail.com>
2024-06-23 18:11:04 +0900

    #19 FEAT: 게시글 등록 API 작성 중

--- src/domain/posts/dto/create-post.dto.ts
+++ src/domain/posts/dto/create-post.dto.ts
@@ -1,17 +1,23 @@
-import { IsNotEmpty, IsOptional, IsString, IsArray } from 'class-validator';
+import { IsNotEmpty, IsOptional, IsString, IsArray} from 'class-validator';
 
 export class CreatePostDto {
     @IsString()
+    @IsNotEmpty({message: '제목을 입력해주세요'})
     title: string;
 
     @IsString()
+    @IsNotEmpty({message: '내용을 입력해주세요'})
     content: string;
 
     @IsArray()
+    @IsNotEmpty({message: '태그를 입력해주세요(빈 배열이라도)'})
     tags: string[];
 
     @IsArray()
+    @IsNotEmpty({message: '이미지를 입력해주세요(빈 배열이라도)'})
     images: string[];
 
-    categoryId: string;
+    @IsString()
+    @IsOptional()
+    categoryId?: string;
 }
