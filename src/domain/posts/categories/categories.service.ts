9b7eb211d9009665966d0854f7ede88308228dde
JongDeug <jongdeug2021@gmail.com>
2024-07-01 19:16:15 +0900

    #35 FEAT: 카테고리 수정 API 로직 작성 중

--- src/domain/posts/categories/categories.service.ts
+++ src/domain/posts/categories/categories.service.ts
@@ -18,9 +18,13 @@ export class CategoriesService {
     }
 
     async updateCategory(name: string, dto: UpdateCategoryDto) {
-        // I. 카테고리가 존재 여부 확인
-        const isExist = await database.category.findUnique({ where: { name } });
-        if (!isExist) throw new CustomError(404, 'Not Found', '카테고리를 찾을 수 없습니다');
+        // I. 기존 카테고리의 존재 여부 확인
+        const isTargetCategoryExist= await database.category.findUnique({ where: { name } });
+        if (!isTargetCategoryExist) throw new CustomError(404, 'Not Found', '바꾸려는 카테고리를 찾을 수 없습니다');
+
+        // I. 새로운 카테고리의 존재 여부 확인
+        const isNewCategoryExists = await database.category.findUnique({ where: { name: dto.name } });
+        if (isNewCategoryExists) throw new CustomError(409, 'Conflict', '업데이트 하려는 카테고리가 이미 존재합니다');
 
         // I. 카테고리 업데이트
         await database.category.update({
