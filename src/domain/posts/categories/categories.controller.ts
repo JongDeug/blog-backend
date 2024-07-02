d09e52c6c4784b5ca292d6b617a57ddde628fbb7
JongDeug <jongdeug2021@gmail.com>
2024-07-02 17:46:16 +0900

    #37 FEAT: 카테고리 삭제 API 작성 완료, 유닛 테스트 작성 완료

--- src/domain/posts/categories/categories.controller.ts
+++ src/domain/posts/categories/categories.controller.ts
@@ -17,6 +17,7 @@ export class CategoriesController {
     init() {
         this.router.post('/', validateDto(CreateCategoryDto), this.createCategory.bind(this));
         this.router.patch('/:name', validateDto(UpdateCategoryDto), this.updateCategory.bind(this));
+        this.router.delete('/:name', this.deleteCategory.bind(this));
     }
 
     async createCategory(req: Request, res: Response, next: NextFunction) {
@@ -43,6 +44,23 @@ export class CategoriesController {
 
             // I. categoriesService.updateCategory 호출
             await this.categoriesService.updateCategory(name, req.body);
+
+            res.status(200).json({});
+        } catch (err) {
+            next(err);
+        }
+    }
+
+    async deleteCategory(req: Request, res: Response, next: NextFunction) {
+        try {
+            // I. JWT 확인
+            if (!req.user) return next(new CustomError(401, 'Unauthorized', '로그인을 진행해주세요'));
+
+            // I. param 가져오기
+            const { name } = req.params;
+
+            // I. categoriesService.deleteCategory 호출
+            await this.categoriesService.deleteCategory(name);
 
             res.status(200).json({});
         } catch (err) {
