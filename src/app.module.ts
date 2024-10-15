a6829d0ea1063f9dcea681bacec6cb026ea89f47
JongDeug <jongdeug2021@gmail.com>
2024-10-15 16:52:55 +0900

    Migration: Prisma, 환경변수 세팅

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,29 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './prisma/prisma.module';
+import * as Joi from 'joi';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        ENV: Joi.string().valid('dev', 'prod').required(),
+        SERVER_PORT: Joi.number().required(),
+        DB_URL: Joi.string().required(),
+        DB_TYPE: Joi.string().valid('mysql').required(),
+        DB_PORT: Joi.number().required(),
+        DB_PWD: Joi.number().required(),
+        REDIS_URL: Joi.string().required(),
+        REDIS_PORT: Joi.number().required(),
+        REDIS_PWD: Joi.number().required(),
+        PASSWORD_SALT: Joi.number().required(),
+        JWT_SECRET: Joi.string().required(),
+        MAIL_ID: Joi.string().required(),
+        MAIL_PWD: Joi.string().required(),
+      }),
       isGlobal: true,
     }),
+    PrismaModule,
   ],
   controllers: [],
   providers: [],
