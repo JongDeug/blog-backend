7048c6d1cb2115bffb82e1d2d770e56aa6586d8e
JongDeug <jongdeug2021@gmail.com>
2024-11-30 23:25:34 +0900

    FEAT: Authorization 데코레이터 에러 쓰로잉 추가

--- src/auth/decorator/authorization.decorator.ts
+++ src/auth/decorator/authorization.decorator.ts
@@ -1,8 +1,16 @@
-import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import {
+  BadRequestException,
+  createParamDecorator,
+  ExecutionContext,
+} from '@nestjs/common';
 
 export const Authorization = createParamDecorator(
   (data: any, context: ExecutionContext) => {
     const req = context.switchToHttp().getRequest();
+
+    if (!req.headers['authorization']) {
+      throw new BadRequestException('token을 입력해주세요 ');
+    }
 
     return req.headers['authorization'];
   },
