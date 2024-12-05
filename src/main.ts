472370313a15e5dfd851fec39f5c94bcb1c01b21
JongDeug <jongdeug2021@gmail.com>
2024-12-05 21:14:57 +0900

    CHORE: update cors

--- src/main.ts
+++ src/main.ts
@@ -10,11 +10,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: [
-      'https://localhost:5173',
-      'https://localhost:4173',
-      'https://localhost:3000',
-    ],
+    origin: ['https://jongdeug.port0.org'],
     credentials: true,
     exposedHeaders: ['Authorization'],
   });
