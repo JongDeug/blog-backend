c79f79797cef759d206de0358d85c250ee1312bb
JongDeug <jongdeug2021@gmail.com>
2025-02-13 13:26:27 +0900

    FEAT: RSS 구독 보완

--- src/common/common.controller.spec.ts
+++ src/common/common.controller.spec.ts
@@ -1,15 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CommonController } from './common.controller';
+import { mock } from 'jest-mock-extended';
+import { CommonService } from './common.service';
 
 describe('CommonController', () => {
   let commonController: CommonController;
+  // let commonService: MockProxy<CommonService>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [CommonController],
+      providers: [{ provide: CommonService, useValue: mock<CommonService>() }],
     }).compile();
 
     commonController = module.get<CommonController>(CommonController);
+    // commonService = module.get(CommonService);
   });
 
   it('should be defined', () => {
